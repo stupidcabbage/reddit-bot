672b23f39e0b4b27f64e3e4265a81971b2f113e2
stupidcabbage <vanya@gutche.ru>
2023-08-08 13:12:30 +0300

    add dataclasses

--- reddit-bot/reddit/api.py
+++ reddit-bot/reddit/api.py
@@ -2,6 +2,7 @@ import praw
 from services.translate import translate_text as _
 from dotenv import load_dotenv
 import os
+from dataclasses import dataclass
 
 from typing import List, Dict
 
@@ -15,26 +16,38 @@ reddit = praw.Reddit(
         user_agent=os.getenv("REDDIT_USER_AGENT")
         )
 
+@dataclass
+class Media:
+    media_url: str
+    format_file: str
 
-def get_media_from_post(post) -> List[str]:
+
+@dataclass
+class Post:
+    title: str
+    description: str
+    media: list[Media]
+
+
+def get_media_from_post(post) -> list[Media]:
     """Возвращает все медиа файлы из поста."""
     media_url = []
     if post.secure_media:
         format_file = post.secure_media[
                 "reddit_video"]["scrubber_media_url"].split(".")[-1]
-        return [{"media": post.secure_media['reddit_video']['fallback_url'],
-                 "format_file": format_file}]
+        url = post.secure_media["reddit_video"]["fallback_url"]
+        return [Media(media_url=url, format_file=format_file)]
     elif hasattr(post, "preview"):
         for media in post.preview['images']:
             url = media["source"]["url"]
             format_file = url.split("?auto")[0].split(".")[-1]
-            media_url.append({"media": url, "format_file": format_file})
+            media_url.append(Media(media_url=url, format_file=format_file))
         return media_url
     elif hasattr(post, "media_metadata"):
         for media in post.media_metadata:
             url = post.media_metadata[media]['s']['u']
             format_file = post.media_metadata[media]["m"].split("/")[-1]
-            media_url.append({"media": url, "format_file": format_file})
+            media_url.append(Media(media_url=url, format_file=format_file))
         return media_url
     return []
 
@@ -47,11 +60,7 @@ def get_new_posts_from_subreddit(sreddit: str,
     for post in reddit_posts:
         title = _(post.title)
         description = _(post.selftext)
-        media_url = get_media_from_post(post)
-        posts.append(
-                {"title": title,
-                 "description": description,
-                 "media": media_url}
-                )
+        media = get_media_from_post(post)
+        posts.append(Post(title=title, description=description, media=media))
     return posts
 
